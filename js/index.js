7c76102ab973e8de113e2abe2a39c192ed5c84cc
joao <joaovsaraivasousa@gmail.com>
2022-10-12 16:06:06 -0300

    criando o js da pagina, alguns ajustes no css para o botao reagir corretamente

---
+++ js/index.js
@@ -0,0 +1,25 @@
+const ratingElements = document.querySelectorAll("p.rating_element");
+const linkToCompletedRating = document.querySelector("a.submit_link");
+
+ratingElements.forEach((ratingElement) => {
+  ratingElement.addEventListener("click", function () {
+    resetRatingElements();
+    applyNewParamToCompletedRating(ratingElement.textContent);
+    ratingElement.style.backgroundColor = "hsl(217, 12%, 63%)";
+    ratingElement.style.color = "white";
+  });
+});
+
+function resetRatingElements() {
+  ratingElements.forEach((ratingElement) => {
+    ratingElement.style.backgroundColor = null;
+    ratingElement.style.color = null;
+  });
+}
+
+function applyNewParamToCompletedRating(param) {
+  linkToCompletedRating.setAttribute(
+    "href",
+    `http://127.0.0.1:5500/completed-rating.html?q=${param}`
+  );
+}
